a186b2e3a4089ac85c971b1b877fb88f2391cdc3
Kireev Konstantin <kireevkonstantin11@gmai.com>
2023-03-12 17:01:02 +0400

    Выделил в отдельную функцию приводение строки входных данных к массиву значений

---
+++ YetAnotherPerceptron/StaticHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace YetAnotherPerceptron
+{
+    public class StaticHelpers
+    {
+        /// <summary>
+        /// Приводит строку входных данных к массиву значений
+        /// </summary>
+        /// <param name="inputString">Строка входных данных</param>
+        /// <param name="separator">Разделитель значений</param>
+        /// <param name="culture">
+        /// Культура разделителя целой и десятичной части 
+        /// (по умолчанию точка)
+        /// </param>
+        /// <returns>Массив чисел с плавающей точкой</returns>
+        public static double[] StringToDoubleValues(string inputString, char separator = ',', string culture = "en-us")
+        {
+            List<double> values = new List<double>();
+
+            var stringValues = inputString.Split(separator);
+
+            foreach (var stringValue in stringValues)
+            {
+                values.Add(double.Parse(stringValue, new CultureInfo(separator)));
+            }
+
+            return values.ToArray();
+        }
+    }
+}
